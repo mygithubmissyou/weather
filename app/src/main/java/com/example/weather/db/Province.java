4db28d71213a0a24b10907e1b193b155964ca5d7
jack <jackli@gmail.com>
2017-09-22 17:21:32 +0800

    添加数据库和表

---
+++ app/src/main/java/com/example/weather/db/Province.java
@@ -0,0 +1,37 @@
+package com.example.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by jack on 2017/9/22.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
